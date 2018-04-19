ff90d405855abb411afb02c29483182bae2a6a31
iRecep <solidrevolution@gmail.com>
2018-04-19 20:36:48 +0300

    refactor out implementations with temp arrays, add some lessons learned

--- Burrows-Wheeler-Transformation/README.md
+++ Burrows-Wheeler-Transformation/README.md
@@ -3,3 +3,9 @@
 https://www.codewars.com/kata/burrows-wheeler-transformation
 
 ## Lessons learned
+
+my first attempt involved creating temporary arrays
+as the input gets bigger memory consumption of the functions will increase exponentially.
+
+[ ] it should be possible to optimize the functions so that they do not use tmp arrays.
+[ ] also the sort performance is a big factor here do calculations with different data
--- Burrows-Wheeler-Transformation/solution.js
+++ Burrows-Wheeler-Transformation/solution.js
@@ -1,39 +1,42 @@
 "use strict";
 
-module.exports = {
-  encode: function encode(s) {
-    if (s === null || s === undefined) return "";
+function encodeWithTempArray(s) {
+  if (s === null || s === undefined) return "";
 
-    let tmp = s;
-    let cache = [];
+  let tmp = s;
+  let cache = [];
 
-    for (let index = 0; index < s.length; index++) {
-      tmp = tmp.charAt(tmp.length - 1) + tmp.slice(0, tmp.length - 1);
-      cache.push(tmp);
+  for (let index = 0; index < s.length; index++) {
+    tmp = tmp.charAt(tmp.length - 1) + tmp.slice(0, tmp.length - 1);
+    cache.push(tmp);
+  }
+  cache.sort();
+
+  var lastColumn = cache.reduce(selectLastChar, "");
+  var rowIndex = cache.findIndex(el => el === s);
+
+  function selectLastChar(acc, cValue) {
+    return acc + cValue[cValue.length - 1];
+  }
+
+  return [lastColumn, rowIndex];
+}
+
+function decodeWithTempArray(s, i) {
+  if (!s) return "";
+
+  let cache = [];
+
+  for (let j = 0; j < s.length; j++) {
+    for (let i = 0; i < s.length; i++) {
+      cache[i] = s[i] + (cache[i] || "");
     }
     cache.sort();
-
-    var lastColumn = cache.reduce(selectLastChar, "");
-    var rowIndex = cache.findIndex(el => el === s);
-
-    function selectLastChar(acc, cValue) {
-      return acc + cValue[cValue.length - 1];
-    }
-
-    return [lastColumn, rowIndex];
-  },
-
-  decode: function decode(s, i) {
-    if (!s) return "";
-
-    let cache = [];
-
-    for (let j = 0; j < s.length; j++) {
-      for (let i = 0; i < s.length; i++) {
-        cache[i] = s[i] + (cache[i] || "");
-      }
-      cache.sort();
-    }
-    return cache[i];
   }
+  return cache[i];
+}
+
+module.exports = {
+  encode: encodeWithTempArray,
+  decode: decodeWithTempArray
 };
